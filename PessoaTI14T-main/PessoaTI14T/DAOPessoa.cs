9b47589d915a393b5c37e1352c414a53fa091133
fialhoMatheus1 <104318023+fialhoMatheus1@users.noreply.github.com>
2022-06-13 17:11:09 -0300

    Add files via upload

---
+++ PessoaTI14T-main/PessoaTI14T/DAOPessoa.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace PessoaTI14T
+{
+    class DAOPessoa
+    {
+        public MySqlConnection conexao;
+        public string dados;
+        public string comando;
+        public string resultado;
+        //declarar os vetores
+        public int[] vetorCodigo;
+        public long[] vetorCPF;
+        public string[] vetorNome;
+        public string[] vetorTelefone;
+        public string[] vetorEndereco;
+        public int i;//declarando o contador do for e do while
+        public int contador;//declarando o contador pra contar os loops do while
+        public string msg;//declarando a variavel pra guardar mensagem dos dados guardados no banco
+        public int contarCodigo;//contador exclusivo para codigos
+
+        public DAOPessoa()
+        {
+            conexao = new MySqlConnection("server=localhost;DataBase=turmaTI14T;Uid=root;password=");
+            try
+            {
+                conexao.Open();//Tentando conectar ao BD
+                MessageBox.Show("Conectado com Sucesso!");
+            }catch(Exception erro)
+            {
+                MessageBox.Show("Algo deu errado!\n\n" + erro);//Enviando a mesagem de erro aos usuários
+                conexao.Close();//fechando a conexão com o banco de dados
+            }
+        }//fim do DAOPessoa
+
+        public void Inserir(long cpf, string nome, string telefone, string endereco)
+        {
+            try
+            {
+                //Preparar os dados para inserir no banco
+                dados = "('','" + cpf + "','" + nome + "','" + telefone + "','" + endereco + "')";
+                comando = "Insert into pessoaTI14T(codigo, cpf, nome, telefone, endereco) values " + dados;
+
+                //Executar o comando na base de dados
+                MySqlCommand sql = new MySqlCommand(comando, conexao);
+                resultado = "" + sql.ExecuteNonQuery();
+                MessageBox.Show(resultado + " Linha Afetada!");
+            }catch(Exception erro)
+            {
+                MessageBox.Show("Algo deu errado!\n\n" + erro);
+            }
+        }//fim do método inserir
+
+        public void PreencherVetor()
+        {
+            string query = "select * from pessoaTI14T";//comand para coletar todos os dados do banco
+
+            //instanciando os vetores
+            vetorCodigo = new int[100];
+            vetorCPF = new long[100];
+            vetorNome = new string[100];
+            vetorTelefone = new string[100];
+            vetorEndereco = new string[100];
+
+            //preencher os vetores previamente criados, ou seja, da-los valores inicias
+            for (i = 0; i < 100; i++)
+            {
+                vetorCodigo[i] = 0;
+                vetorCPF[i] = 0;
+                vetorNome[i] = "";
+                vetorTelefone[i] = "";
+                vetorEndereco[i] = "";
+            }//fim do for
+
+            //realizar os comandos de consulta ao banco de dados
+            MySqlCommand coletar = new MySqlCommand(query, conexao);
+            //ler os dados de acordo com o que esta no banco
+            MySqlDataReader leitura = coletar.ExecuteReader(); //variavel 'leitura' faz uma consulta ao banco
+
+            i = 0;//declaracao do contador 0 pro while
+            contador = 0;//declaracao do contador 0 pro while
+            contarCodigo = 0;//instanciando o contador para o codigo
+            //preencher vetores com dados do banco de dados
+            while (leitura.Read())//enquanto leitura for verdadeiro executa while
+            {
+                vetorCodigo[i] = Convert.ToInt32(leitura["codigo"]);
+                vetorCPF[i] = Convert.ToInt64(leitura["cpf"]);
+                vetorNome[i] = leitura["nome"] + "";//concateno com aspsa para converter pra string
+                vetorTelefone[i] = leitura["telefone"] + "";//concateno com aspsa para converter pra string
+                vetorEndereco[i] = leitura["endereco"] + "";//concateno com aspsa para converter pra string
+                contarCodigo = contador;//armazenando a ultima posição do contador
+                i++;//contador sai da posicao 0 e vai se incrementando
+                contador++;//contar os loops do while
+
+
+            }//fim do while
+
+            leitura.Close();//fechar conexao e leitura do banco de dados
+
+
+        }//fim do metodo preencher vetor
+
+        //criar um consultar tudo por MessageBox
+        public string ConsultarTudo()
+        {
+            PreencherVetor();//primeira coisa => preencher vetores com dados do DB
+
+            msg = "";
+            for (i = 0; i < contador; i++)
+            {
+                msg += "Codigo: " + vetorCodigo[i] +
+                    ", CPF : " + vetorCPF[i] +
+                    ", Nome; " + vetorNome[i] +
+                    ", Telefone: " + vetorTelefone[i] +
+                    ", Endereço: " + vetorEndereco[i] +
+                    "\n\n";
+            }//fim fo for
+            return msg;//retorna todos os dados guardados na variavel 'msg'
+
+        }//fim do metodo consultar tudo
+
+        public int ConsultarCodigo()
+        {
+            PreencherVetor();//preencher os vetores com os dados do banco
+            return vetorCodigo[contarCodigo];
+
+        }//fim do metodo consultar codigo
+
+        public long ConsultarCPF(int cod)
+        {
+            PreencherVetor();
+            for(i=0; i < contador; i++)
+            {
+                if (vetorCodigo[i] == cod)
+                {
+                    return vetorCPF[i];
+                }//fim do if
+            }//fim do for
+            return -1;
+        }//fim do metodo consultar cpf
+
+        public string ConsultarNome(int cod)
+        {
+            PreencherVetor();
+            for (i = 0; i < contador; i++)
+            {
+                if(vetorCodigo[i] == cod)
+                {
+                    return vetorNome[i];
+                }//fim du if
+            }//fim do if
+            return "Nome não encontrado.";
+        }//fim do consultar nome
+
+        public string ConsultarTelefone(int cod)
+        {
+            PreencherVetor();
+            for(i = 0; i < contador; i++)
+            {
+                if(vetorCodigo[i] == cod)
+                {
+                    return vetorTelefone[i];
+                }//fim do if
+            }//fim do for
+            return "Telefone não encontrado.";
+        }//fim do consultar telefone
+
+        public string ConsultarEndereco(int cod)
+        {
+            PreencherVetor();
+            for (i = 0; i < contador; i++)
+            {
+                if (vetorCodigo[i] == cod)
+                {
+                    return vetorEndereco[i];
+                }//fim do if
+            }//fim do for
+            return "Endereço não encontrado.";
+        }//fim do consultar endereço
+
+        public void Atualizar(int cod, string campo, string novoDado)
+        {
+            try
+            {
+                string query = "update pessoaTI14T set " + campo + " = '" + novoDado + "' where codigo = '" + cod + "'";
+
+                MySqlCommand sql = new MySqlCommand(query, conexao);
+                string resultado = "" + sql.ExecuteNonQuery();
+                MessageBox.Show(resultado + " Linha afetada! ");
+            }
+            catch(Exception erro)
+            {
+                MessageBox.Show("" + erro);
+            }
+        }//fim do metodo atualizar
+
+        //converter cpf de string para long
+        public void Atualizar(int cod, string campo, long novoDado)
+        {
+            try
+            {
+                string query = "update pessoaTI14T set " + campo + " = '" + novoDado + "' where codigo = '" + cod + "'";
+
+                MySqlCommand sql = new MySqlCommand(query, conexao);
+                string resultado = "" + sql.ExecuteNonQuery();
+                MessageBox.Show(resultado + " Linha afetada! ");
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("" + erro);
+            }
+        }//fim do metodo atualizar
+
+        public void Deletar(int cod)
+        {
+            try
+            {
+                string query = "delete from pessoaTI14T where codigo = '" + cod + "'";
+
+                MySqlCommand sql = new MySqlCommand(query, conexao);
+                string resultado = "" + sql.ExecuteNonQuery();
+                MessageBox.Show(resultado + " Linha Afetada! ");
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("" + erro);
+            }
+        }//fim do deletar
+    }//fim da classe
+}//fim do projeto
---
+++ PessoaTI14T-main/PessoaTI14T/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PessoaTI14T
+{
+    public partial class Form1 : Form
+    {
+        DAOPessoa pessoa;
+        public Form1()
+        {
+            InitializeComponent();
+            pessoa = new DAOPessoa();//Abrindo a conexão com o Banco de Dados
+            textBox1.Text = Convert.ToString( pessoa.ConsultarCodigo() + 1);//mostra o proximo codigo na tela depois do ultimo codigo cadastrado, por isso o +1
+            textBox1.ReadOnly = true;//bloqueando o codigo no primeiro acesso
+        }//Fim do método construtor
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Limpar()
+        {
+            textBox1.Text = "" + pessoa.ConsultarCodigo();//codigo
+            maskedTextBox1.Text = "";//cpf
+            textBox2.Text = "";//nome
+            maskedTextBox2.Text = "";//telefone
+            textBox4.Text = "";//endereço
+
+        }//fim do metodo Limpar tela
+
+        public void AtivarCampos()
+        {
+                textBox1.ReadOnly = false;//codigo
+                maskedTextBox1.ReadOnly = true;//cpf
+                textBox2.ReadOnly = true;//nome
+                maskedTextBox2.ReadOnly = true;//telefone
+                textBox4.ReadOnly = true;//endereço
+        }//fim do metodo ativar campos
+
+        public void InativarCampos()
+        {
+            textBox1.ReadOnly = true;//codigo
+            maskedTextBox1.ReadOnly = false;//cpf
+            textBox2.ReadOnly = false;//nome
+            maskedTextBox2.ReadOnly = false;//telefone
+            textBox4.ReadOnly = false;//endreço
+        }//fim do metodo inativar campos
+
+        private void Cadastrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(textBox1.ReadOnly == false)
+                {
+                    Limpar();
+                    InativarCampos();
+                }
+                else
+                {
+                    long cpf = TratarCPF(maskedTextBox1.Text);//Coletando o dado do campo CPF
+                    string nome = textBox2.Text;//Coletando o dado do campo nome
+                    string telefone = maskedTextBox2.Text;//Coletando o dado do campo telefone
+                    string endereco = textBox4.Text;//Coletando o dado do campo Endereço
+                                                    //Chamar o método inserir que foi criado na classe DAOPessoa
+                    pessoa.Inserir(cpf, nome, telefone, endereco);//Inserir no banco os dados do formulário
+                    Limpar();//limpa os campos
+                }
+            }catch(Exception erro)
+            {
+                MessageBox.Show("" + erro);
+            }
+        }//fim do botão cadastrar
+
+        public long TratarCPF(string cpf)
+        {
+            string tratamento = cpf.Replace(",", "");
+            tratamento = tratamento.Replace("-", "");
+            return Convert.ToInt64(tratamento);
+        }//fim do tratar cpf
+
+        private void Consultar_Click(object sender, EventArgs e)
+        {
+            if(textBox1.ReadOnly == true)
+            {
+                AtivarCampos();
+            }
+            else
+            {
+                maskedTextBox1.Text = "" + pessoa.ConsultarCPF(Convert.ToInt32(textBox1.Text));//preenchedo campo CPF
+                textBox2.Text = pessoa.ConsultarNome(Convert.ToInt32(textBox1.Text));//preenchedo campo NOME
+                maskedTextBox2.Text = pessoa.ConsultarTelefone(Convert.ToInt32(textBox1.Text));//preenchendo campo TELEFONE
+                textBox4.Text = pessoa.ConsultarEndereco(Convert.ToInt32(textBox1.Text));//preenchendo campo ENDEREÇO
+            }
+            
+        }//fim do botão consultar
+
+        private void Atualizar_Click(object sender, EventArgs e)
+        {
+            //mesmos metodos do consultar, porem acrescentar if/else:
+            if(textBox1.Text == "")//se textBox.Text (nome, no caso) esta vazio, entao preenche com os dados do banco
+            {
+                maskedTextBox1.Text = "" + pessoa.ConsultarCPF(Convert.ToInt32(textBox1.Text));//preenchedo campo CPF
+                textBox2.Text = pessoa.ConsultarNome(Convert.ToInt32(textBox1.Text));//preenchedo campo NOME
+                maskedTextBox2.Text = pessoa.ConsultarTelefone(Convert.ToInt32(textBox1.Text));//preenchendo campo TELEFONE
+                textBox4.Text = pessoa.ConsultarEndereco(Convert.ToInt32(textBox1.Text));//preenchendo campo ENDEREÇO
+            }
+            else//se nao estiver vazio, atualizar com novos dados:
+            {
+                //apos repetir os metodos, atualiza-los:
+                pessoa.Atualizar(Convert.ToInt32(textBox1.Text), "cpf", TratarCPF(maskedTextBox1.Text));//atualizar cpf
+                pessoa.Atualizar(Convert.ToInt32(textBox1.Text), "nome", textBox2.Text); //atualizar nome
+                pessoa.Atualizar(Convert.ToInt32(textBox1.Text), "telefone", maskedTextBox2.Text);//atualizar telefone
+                pessoa.Atualizar(Convert.ToInt32(textBox1.Text), "endereco", textBox4.Text);//atualizar endereço
+                Limpar();
+            }
+        }//fim do botão Atualizar
+
+        private void Excluir_Click(object sender, EventArgs e)
+        {
+            pessoa.Deletar(Convert.ToInt32(textBox1.Text));
+        }//fim do botão Excluir
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }//textbox de código
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }//maskedTextBox de CPF
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }//textBox de nome
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }//MaskedTextBox de Telefone
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }//TextBox de Endereço
+    }//fim da classe
+}//fim do projeto
